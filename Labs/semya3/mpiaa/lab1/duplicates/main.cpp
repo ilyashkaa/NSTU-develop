437fd62ab00798ba8f8e4f338cec90ee16506c07
ilyashkaa <ilyasailer31@gmail.com>
2024-11-02 00:43:09 +0700

    123

--- Labs/semya3/mpiaa/lab1/duplicates/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "duplicates.h"
-
-#define CATCH_CONFIG_RUNNER
-#include "../catch.hpp"
-
-int main(int argc, char **argv) {
-    int result = Catch::Session().run(argc, argv);
-    return result;
-}
